51b91556ac133b4535d3758078f7fb44673ea455
swap357 <swapnilpatel357@gmail.com>
2025-06-17 12:01:52 -0700

    test failing test_sysinfo.TestSysInfo

--- buildscripts/condarecipe.local/bld.bat
+++ buildscripts/condarecipe.local/bld.bat
@@ -10,6 +10,9 @@ if not exist "%VSINSTALLDIR%" (
 echo "Found VS 2022 in %VSINSTALLDIR%"
 call "%VSINSTALLDIR%VC\Auxiliary\Build\vcvarsall.bat" x64
 
+REM Ensure Windows system directories are in PATH for wmic and other tools
+set PATH=%SystemRoot%\System32;%SystemRoot%\System32\Wbem;%SystemRoot%\System32\WindowsPowerShell\v1.0;%PATH%
+
 %PYTHON% setup.py build install --single-version-externally-managed --record=record.txt
 
 exit /b %errorlevel%
